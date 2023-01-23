d3742bf65be33762cbfcb74d3dd4906ab36c368e
Daniel Moura <daniel-moura@outlook.com>
2023-01-23 11:59:18 -0300

    feat: implement findByTitleContaining using query builder

--- src/modules/games/repositories/implementations/GamesRepository.ts
+++ src/modules/games/repositories/implementations/GamesRepository.ts
@@ -14,8 +14,9 @@ export class GamesRepository implements IGamesRepository {
 
   async findByTitleContaining(param: string): Promise<Game[]> {
     return this.repository
-      .createQueryBuilder()
-      // Complete usando query builder
+      .createQueryBuilder('game')
+        .where('game.title ILIKE :term', { term: `%${param}%` })
+        .getMany();      
   }
 
   async countAllGames(): Promise<[{ count: string }]> {
